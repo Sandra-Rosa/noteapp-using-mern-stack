0f9d70d61318cd7c8e9a461483462b0c5ef78073
Sandra-Rosa <sandraantony2002@gmail.com>
2022-08-18 19:16:04 +0530

    Installed a neccesary npm libraries

---
+++ server.js
@@ -0,0 +1,11 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+const PORT = process.env
